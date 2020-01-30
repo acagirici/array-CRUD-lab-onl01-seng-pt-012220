d4589da1875189e675417996061b6b62f2bf20b9
Arif Cagirici <acagirici23@gmail.com>
2020-01-30 00:03:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  my_arr = []
 end
 
 def create_an_array
-  
+  my_arr = ["James","Bobby","Jessica","Jeniffer"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  my_arr = my_arr.push("Jones")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  my_arr.unshift("Superman")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  my_arr.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  my_arr.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  my_arr[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  my_arr[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  my_arr[-1]
 end
